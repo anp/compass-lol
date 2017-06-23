a5c6846bfc07fb5aeaa839adbd1351ad8bb8d80b
Adam Perry <adam.n.perry@gmail.com>
2017-06-23 12:38:15 -0700

    get the heading

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
+// @flow
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { KeepAwake, Location } from 'expo';
+import debounce from 'lodash.debounce';
+
+let locationWatcher = null;
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { heading: 0, accuracy: 0 };
+
+    this.setStateDebounced = debounce(this.setState, 5);
+  }
+
+  componentWillMount() {
+    if (!locationWatcher) {
+      locationWatcher = Location.watchHeadingAsync(({ magHeading, trueHeading, accuracy }) => {
+      console.log(magHeading, trueHeading, accuracy);
+
+      this.setStateDebounced({ heading: trueHeading || magHeading, accuracy });
+    });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up main.js to start working on your app!</Text>
+        <KeepAwake/>
+        <Text>{this.state.heading}</Text>
+        <Text>{this.state.accuracy}</Text>
       </View>
     );
   }
